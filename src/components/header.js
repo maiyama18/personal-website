070ad140ef5ba1ed354fda96a7356058228dfbc2
yam-net <myam0118@gmail.com>
2019-01-12 15:43:18 +0900

    init

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Header = () => (
+    <div>
+        <h1>Portfolio</h1>
+        <Link to="/diary">diary</Link>
+    </div>
+);
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from '../components/header';
+
+const Index = () => (
+    <div>
+        <Header/>
+        <h2>Profile</h2>
+        <ul>
+            <li>M. Y.</li>
+            <li>1990/01/18</li>
+        </ul>
+
+        <h2>Web sites</h2>
+        <ul>
+            <li>website1</li>
+            <li>website2</li>
+            <li>website3</li>
+        </ul>
+    </div>
+);
+
+export default Index;
