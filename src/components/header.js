4be9e6f64c85dc495fb64d6dbe6543a9f05d4e2e
muiscript <myam0118@gmail.com>
2019-01-13 13:29:35 +0900

    add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Link, } from 'gatsby';
+import { Link } from 'gatsby';
+import { Container, Menu } from 'semantic-ui-react';
 
 export default ({ title }) => (
-    <div>
-        <h1>{title}</h1>
-        <Link to="/diary">diary</Link>
-    </div>
+    <Menu attached inverted borderless size={'huge'} color={'grey'} style={{ marginBottom: '1rem' }}>
+        <Container>
+            <Menu.Item>
+                <Link to="/">{title}</Link>
+            </Menu.Item>
+
+            <Menu.Menu position={'right'}>
+                <Menu.Item>
+                    <Link to="/diary">diary</Link>
+                </Menu.Item>
+            </Menu.Menu>
+
+        </Container>
+    </Menu>
 );
 
