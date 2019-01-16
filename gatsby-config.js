47c9451a9c9d9c3c62ab05f15e465e1ef83d3e47
yam-net <myam0118@gmail.com>
2019-01-17 00:02:33 +0900

    add rss/xml for diary and blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,30 +30,86 @@ module.exports = {
             options: {
                 feeds: [
                     {
-                        title: 'title',
-                        output: '/rss.xml',
+                        title: "muiscript's diary",
+                        output: '/diary/rss.xml',
                         serialize: ({ query: { site, allContentfulDiary } }) => {
                             return allContentfulDiary.edges.map(({ node }) => {
+                                const formattedDate = node.date.replace(/-/g, '');
                                 return {
-                                    url: `${site.siteMetadata.siteUrl}/diary/${node.date}`,
+                                    url: `${site.siteMetadata.siteUrl}/diary/${formattedDate}`,
+                                    guid: `${site.siteMetadata.siteUrl}/diary/${formattedDate}`,
+                                    date: node.date,
+                                    title: formattedDate,
+                                    description: node.content.childMarkdownRemark.html,
                                 }
                             })
                         },
                         query: `{
-                      allContentfulDiary {
-                        edges {
-                          node {
-                            date
-                            content {
-                              childMarkdownRemark {
-                                html
+                          site {
+                            siteMetadata {
+                              title
+                              siteUrl
+                            }
+                          }
+                          allContentfulDiary(
+                            limit: 1000,
+                            sort: { fields: [date], order: DESC },
+                          ) {
+                            edges {
+                              node {
+                                date
+                                content {
+                                  childMarkdownRemark {
+                                    html
+                                  }
+                                }
                               }
                             }
                           }
                         }
-                      }
-                    }`,
-                    }
+                        `,
+                    },
+                    {
+                        title: "muiscript's blog",
+                        output: '/blog/rss.xml',
+                        serialize: ({ query: { site, allContentfulBlog } }) => {
+                            return allContentfulBlog.edges.map(({ node }) => {
+                                return {
+                                    url: `${site.siteMetadata.siteUrl}/blog/${node.id}`,
+                                    guid: `${site.siteMetadata.siteUrl}/blog/${node.id}`,
+                                    date: node.postedAt,
+                                    title: node.title,
+                                    description: node.body.childMarkdownRemark.html,
+                                }
+                            })
+                        },
+                        query: `{
+                          site {
+                            siteMetadata {
+                              title
+                              siteUrl
+                            }
+                          }
+                          allContentfulBlog(
+                           limit: 1000,
+                           sort: { fields: [postedAt], order: DESC }
+                          ) {
+                            edges {
+                              node {
+                                id
+                                title
+                                postedAt
+                                body {
+                                  childMarkdownRemark {
+                                    html
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                        `,
+                    },
                 ]
             }
         },
