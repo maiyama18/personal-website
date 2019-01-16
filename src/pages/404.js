3c8c16997e480289ea61804aa8a7cc060a331038
yam-net <myam0118@gmail.com>
2019-01-17 00:48:47 +0900

    improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Layout from '../components/layout';
 
 const NotFound = () => (
-    <div>404 page not found</div>
+    <Layout>
+        <div>404 page not found</div>
+    </Layout>
 );
 
 export default NotFound;
