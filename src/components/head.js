e77aa1613e644e74df52af0bd0e0f0e6e696765b
yam-net <myam0118@gmail.com>
2019-01-19 16:38:36 +0900

    shorten meta description

--- src/components/head.js
+++ src/components/head.js
@@ -5,14 +5,17 @@ export default ({ title, description, pageUrl, imageUrl }) => (
     <Helmet
         title={title}
         meta={[
-            { name: 'description', content: description },
+            { name: 'description', content: description.slice(0, 120) + '...' },
             { name: 'og:title', content: title },
             { name: 'og:type', content: 'blog' },
             { name: 'og:url', content: pageUrl },
             { name: 'og:image', content: imageUrl },
-            { name: 'og:description', content: description },
+            { name: 'og:description', content: description.slice(0, 120) + '...' },
             { name: 'twitter:card', content: 'summary' },
             { name: 'twitter:site', content: '@muiscript' },
+        ]}
+        link={[
+            { rel: 'icon', 'type': 'image/png', href: imageUrl }
         ]}
     />
 )
