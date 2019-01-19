06572b74be671c4405c1a5bb7eff1992a54e0e26
yam-net <myam0118@gmail.com>
2019-01-19 16:47:59 +0900

    remove newline from description

--- src/components/head.js
+++ src/components/head.js
@@ -5,12 +5,12 @@ export default ({ title, description, pageUrl, imageUrl }) => (
     <Helmet
         title={title}
         meta={[
-            { name: 'description', content: description.slice(0, 120) + '...' },
+            { name: 'description', content: description.replace(/\r?\n/g, '').slice(0, 120) + '...' },
             { name: 'og:title', content: title },
             { name: 'og:type', content: 'blog' },
             { name: 'og:url', content: pageUrl },
             { name: 'og:image', content: imageUrl },
-            { name: 'og:description', content: description.slice(0, 120) + '...' },
+            { name: 'og:description', content: description.replace(/\r?\n/g, '').slice(0, 120) + '...' },
             { name: 'twitter:card', content: 'summary' },
             { name: 'twitter:site', content: '@muiscript' },
         ]}
@@ -18,4 +18,4 @@ export default ({ title, description, pageUrl, imageUrl }) => (
             { rel: 'icon', 'type': 'image/png', href: imageUrl }
         ]}
     />
-)
+);
