5145386838d5b6793f78a4c2895ddd33095fc66a
yam-net <myam0118@gmail.com>
2019-01-19 17:13:14 +0900

    ssr react helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
                 ]
             }
         },
+        'gatsby-plugin-react-helmet',
         {
             resolve: 'gatsby-plugin-feed',
             options: {
